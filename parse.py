b7864d962eea1e057cff0c9869c65ce556c76f6e
Ruth Tillman <ruthtillman@gmail.com>
2016-05-19 14:35:02 -0400

    Initial commit of parse.py.

---
+++ parse.py
@@ -0,0 +1,23 @@
+import json
+from jq import jq
+
+def parseData():
+    theFile = raw_input("Ingest file name if in our directory or full filepath and filename: ")
+    with open(theFile) as successfulIngest:
+      metadata = json.load(successfulIngest)
+    pidTitle = jq('.[] | select(.["af-model"] != "GenericFile") |.pid?, .metadata["dc:title"]').transform(metadata, multiple_output=True)
+    writeFile(pidTitle)
+
+def writeFile(pidTitle):
+    outputFile = raw_input("File name: ")
+    f = open(outputFile, 'w')
+    f.write('PID,Title\n')
+    while pidTitle != []:
+      PID = '"' + pidTitle.pop(0) + '"'
+      title = '"' + pidTitle.pop(0) + '"'
+      row = PID + ',' + title + '\n'
+      f.write(row)
+
+    f.close()
+
+parseData()
